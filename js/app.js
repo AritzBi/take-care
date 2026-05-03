abd331d0d3c22ada82ad946139be23201863aecb
AritzBi <aritzbi@gmail.com>
2026-05-03 17:32:17 +0200

    chore: comment asymmetric pre-render in goNext

--- js/app.js
+++ js/app.js
@@ -51,6 +51,7 @@ function submitAnswer(answer) {
 }
 
 function goNext(fromScreen) {
+  // renderResponse/Evidence/Tips are pre-rendered in submitAnswer; only question needs lazy render here
   if (fromScreen === 'screen-definition') {
     renderQuestion(NODES[currentNode]);
   }
